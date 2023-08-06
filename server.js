7c706c4edeb986e37018dd3d570d42ef5c0f4e60
singhsharad529 <singhsharad529@gmail.com>
2023-08-06 13:24:57 +0530

    first commit

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ app.get('/', (req, res) => {
     res.send("<h1>Hi Welcome to my app</h1>")
 });
 
+app.get('/production', (req, res) => {
+    res.send("<h1>Hi Welcome to my app : production</h1>")
+});
+
 
 app.listen(5000, () => {
-    console.log('server is listening on port 5000');
+    console.log('server is listening on port 6000');
 })
