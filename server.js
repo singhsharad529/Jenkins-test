7d4fd169da6a60fc79d23f3ac3fcdaee9b477783
singhsharad529 <singhsharad529@gmail.com>
2023-08-06 15:16:40 +0530

    added home api

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ app.get('/development', (req, res) => {
     res.send("<h1>Hi Welcome to my app : Devlopment</h1>")
 });
 
+app.get('/home', (req, res) => {
+    res.send("<h1>Hi Welcome to my app : Home</h1>")
+});
+
 app.listen(5000, () => {
     console.log('server is listening on port 5000');
 })
